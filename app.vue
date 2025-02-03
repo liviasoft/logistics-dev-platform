b08e0ccf2aac19bcc247ed433b4087fec04ac23b
Bankole <techybanky@gmail.com>
2025-02-03 04:42:44 +0100

    build: add vuetify, pinia, vsonner

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
+<script lang="ts" setup>
+  import { VSonner, toast } from '@neoncoder/vuetify-sonner'
+  import '@neoncoder/vuetify-sonner/style.css'
+</script>
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <VSonner position="bottom-left" :visible-toasts="5" :expand="false" />
+    <VBtn @click="toast('My first toast')">Here is a button</VBtn>
     <h1>Hello World</h1>
     <NuxtWelcome />
   </div>
